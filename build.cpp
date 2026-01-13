4953885fcefe0ea02690ea78599a66cece1192db
YvanCywan <c1yvan@outlook.com>
2026-01-13 16:15:42 +0000

    feat: anvil test

--- build.cpp
+++ build.cpp
@@ -1,11 +1,4 @@
-#include <functional>
-
 #include "anvil/api.hpp"
-
-void anvil::Project::add_executable(const std::string &name, std::function<void(CppApplication &)> config) {
-    application.name = name;
-    config(application);
-}
 
 void configure(anvil::Project& project) {
     project.name = "anvildemo";
@@ -16,6 +9,12 @@ void configure(anvil::Project& project) {
         app.add_source("src/main/main.cpp");
         app.set_compiler(anvil::CompilerId::GCC);
 
+        app.add_include("src");
+    });
+
+    project.add_test("anvil_tests", [](anvil::CppApplication& app) {
+        app.standard = anvil::CppStandard::CPP_20;
+        app.add_source("src/test/test_main.cpp");
         app.add_include("src");
     });
 }
---
+++ src/test/test_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cassert>
+
+int main() {
+    std::cout << "Running tests..." << std::endl;
+
+    assert(1 + 1 == 2);
+
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
+}
